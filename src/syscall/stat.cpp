18d18bdd5df8b6f1815c0011cd1bf25bc842dd44
Nick Wanninger <nickwanninger@gmail.com>
2020-01-04 05:50:42 +0000

    feat: syscall security: validate statbuf

--- src/syscall/stat.cpp
+++ src/syscall/stat.cpp
@@ -7,6 +7,8 @@ int sys::stat(const char *pathname, struct stat *statbuf) { return -1; }
 int sys::fstat(int fd, struct stat *statbuf) {
   auto proc = cpu::proc().get();
 
+  if (!proc->mm.validate_pointer(statbuf, sizeof(*statbuf), VPROT_READ | VPROT_WRITE)) return -1;
+
   auto file = proc->get_fd(fd);
 
   if (!file) return -ENOENT;
