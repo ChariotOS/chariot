365494f6ea07a2f6627d42129f5db5bc05419e1f
Nick Wanninger <nickwanninger@gmail.com>
2020-01-07 01:01:34 +0000

    Change up run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,9 @@
 
 ./sync.sh
 
-qemu-system-x86_64 \
-	-gdb tcp::8256 \
-	-nographic -serial mon:stdio \
-	-m 4G -smp 4 \
-	-hda build/root.img \
-	-device rtl8139 \
+qemu-system-x86_64 -gdb tcp::8256            \
+	-nographic -serial mon:stdio               \
+	-m 4G -smp 4                               \
+	-hda build/root.img                        \
+	-device rtl8139                            \
 	$@
