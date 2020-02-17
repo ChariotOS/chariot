5fa97c885fb97c106b9c8c58f9441a2fec53d130
Nick Wanninger <nickwanninger@gmail.com>
2020-02-17 21:33:55 +0000

    feat: cleanup cal.c

--- user/src/cal/cal.c
+++ user/src/cal/cal.c
@@ -29,48 +29,8 @@ const char *day_names[7] = {"Monday", "Tuesday",  "Wednesday", "Thursday",
 #define SPACE -1   /* used in day array */
 
 static const int days_in_month[2][13] = {
-    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-};
-
-const int sep1752s[MAXDAYS] =
-    {
-        SPACE, SPACE, 1,     2,     14,    15,    16,    17,    18,
-        19,    20,    21,    22,    23,    24,    25,    26,    27,
-        28,    29,    30,    SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-        SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-        SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-},
-          sep1752m[MAXDAYS] =
-              {
-                  SPACE, 1,     2,     14,    15,    16,    17,    18,    19,
-                  20,    21,    22,    23,    24,    25,    26,    27,    28,
-                  29,    30,    SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-                  SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-                  SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-},
-          sep1752js[MAXDAYS] =
-              {
-                  SPACE, SPACE, 245,   246,   258,   259,   260,   261,   262,
-                  263,   264,   265,   266,   267,   268,   269,   270,   271,
-                  272,   273,   274,   SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-                  SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-                  SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-},
-          sep1752jm[MAXDAYS] =
-              {
-                  SPACE, 245,   246,   258,   259,   260,   261,   262,   263,
-                  264,   265,   266,   267,   268,   269,   270,   271,   272,
-                  273,   274,   SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-                  SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-                  SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-},
-          empty[MAXDAYS] = {
-              SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-              SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-              SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-              SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
-              SPACE, SPACE, SPACE, SPACE, SPACE, SPACE,
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},  // non leap-year
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},  // leap-year
 };
 
 /* leap year -- account for gregorian reformation in 1752 */
@@ -87,33 +47,15 @@ const int sep1752s[MAXDAYS] =
 #define leap_years_since_year_1(yr) \
   ((yr) / 4 - centuries_since_1700(yr) + quad_centuries_since_1700(yr))
 
-int julian;
-int mflag = 0;
-int wflag = 0;
-
-void ascii_day(char *, int);
 void center(const char *, int, int);
-void day_array(int, int, int *);
 int day_in_week(int, int, int);
 int day_in_year(int, int, int);
-int week(int, int, int);
-int isoweek(int, int, int);
-void j_yearly(int);
-void monthly(int, int);
-void trim_trailing_spaces(char *);
-void usage(void);
-void yearly(int);
-int parsemonth(const char *);
-
-
 
 void print_month(int month, int year, struct tm *now) {
-
   char header[20];
   snprintf(header, 20, "%s %d", month_names[month - 1], year);
   center(header, 20, 0);
   printf("\n");
-
 
   printf("%s\n", DAY_HEADINGS_S);
 
@@ -131,7 +73,6 @@ void print_month(int month, int year, struct tm *now) {
       d = 0;
     }
 
-
     if (i == now->tm_mday) {
       printf("\x1b[7m%2d\x1b[27m ", i);
     } else {
@@ -140,7 +81,6 @@ void print_month(int month, int year, struct tm *now) {
     d++;
   }
   printf("\n");
-
 }
 
 int main(int argc, char **argv) {
@@ -153,7 +93,6 @@ int main(int argc, char **argv) {
 
   print_month(local_time.tm_mon, local_time.tm_year, &local_time);
 
-
   return 0;
 }
 
@@ -162,29 +101,21 @@ void center(const char *str, int len, int separate) {
   (void)printf("%*s%s%*s", len / 2, "", str, len / 2 + len % 2 + separate, "");
 }
 
+int day_in_week(int day, int month, int year) {
+  long temp;
 
-
-int
-day_in_week(int day, int month, int year)
-{
-	long temp;
-
-	temp = (long)(year - 1) * 365 + leap_years_since_year_1(year - 1)
-	    + day_in_year(day, month, year);
-	if (temp < FIRST_MISSING_DAY)
-		return ((temp - 1 + SATURDAY) % 7);
-	if (temp >= (FIRST_MISSING_DAY + NUMBER_MISSING_DAYS))
-		return (((temp - 1 + SATURDAY) - NUMBER_MISSING_DAYS) % 7);
-	return (THURSDAY);
+  temp = (long)(year - 1) * 365 + leap_years_since_year_1(year - 1) +
+         day_in_year(day, month, year);
+  if (temp < FIRST_MISSING_DAY) return ((temp - 1 + SATURDAY) % 7);
+  if (temp >= (FIRST_MISSING_DAY + NUMBER_MISSING_DAYS))
+    return (((temp - 1 + SATURDAY) - NUMBER_MISSING_DAYS) % 7);
+  return (THURSDAY);
 }
 
-int
-day_in_year(int day, int month, int year)
-{
-	int i, leap;
+int day_in_year(int day, int month, int year) {
+  int i, leap;
 
-	leap = leap_year(year);
-	for (i = 1; i < month; i++)
-		day += days_in_month[leap][i];
-	return (day);
+  leap = leap_year(year);
+  for (i = 1; i < month; i++) day += days_in_month[leap][i];
+  return (day);
 }
