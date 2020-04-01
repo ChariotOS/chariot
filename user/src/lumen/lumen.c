1b7f1625f826f852990f2feb175a37b68f2366e5
Nick Wanninger <nickwanninger@gmail.com>
2020-04-01 13:30:50 -0500

    fix: remove user.ld alignment

--- user/src/lumen/lumen.c
+++ user/src/lumen/lumen.c
@@ -15,22 +15,18 @@ struct rect {
   int w, h;
 };
 
-
 struct window {
-	struct rect pos;
+  struct rect pos;
 
-	// Z ordering
-	struct window *next;
-	struct window *prev;
+  // Z ordering
+  struct window *next;
+  struct window *prev;
 };
 static int fbfd = -1;
 
 static uint64_t frame = 0;
 static struct rect screen_size;
 static uint32_t *buffer = NULL;
-
-
-
 
 #define WIDTH (screen_size.w)
 #define HEIGHT (screen_size.h)
@@ -72,26 +68,49 @@ int main(int argc, char **argv) {
 
 static void blit(void) { write(fbfd, buffer, BSIZE); }
 
+static void set_pixel(int x, int y, int c) {
+  if (x > WIDTH || x < 0 || y > HEIGHT || y < 0) return;
+  buffer[x + y * WIDTH] = c;
+}
 
+void draw_window(struct window *win) {
+  int x, y;
 
-static void compose(void) {
-  // go through and draw the background
-  if (1) {
-    long x = 0;
-    for (unsigned long i = 0; i < WIDTH * HEIGHT; i++) {
-			int color = x & 1 ? 0xFFFFFFFF : 0;
-			buffer[i] = color;
-
-			x++;
-			if (x > WIDTH) {
-				x = 0;
-			}
-
+  for (y = 0; y < win->pos.h; y++) {
+    for (x = 0; x < win->pos.w; x++) {
+      set_pixel(x + win->pos.x, y + win->pos.y, 0x252525);
     }
   }
+}
 
 
 
+static uint32_t seed;  // The state can be seeded with any value.
+// Call next() to get 32 pseudo-random bits, call it again to get more bits.
+// It may help to make this inline, but you should see if it benefits your
+// code.
+static inline uint64_t next_random(void) {
+  uint32_t z = (seed += 0x6D2B79F5UL);
+  z = (z ^ (z >> 15)) * (z | 1UL);
+  z ^= z + (z ^ (z >> 7)) * (z | 61UL);
+  return z ^ (z >> 14);
+}
+static void compose(void) {
+
+	static int i = 0;
+  // set it to some kind of grey
+  memset(buffer, i++, BSIZE);
+
+  struct window win;
+
+  // arbitrary
+  win.pos.w = 160;
+  win.pos.h = 195;
+
+  win.pos.x = next_random() % WIDTH;
+  win.pos.y = next_random() % HEIGHT;
+
+  draw_window(&win);
 
   blit();
 }
