61e38ea5e7eff727048a1372ad7f391fe0dfcde1
Nick Wanninger <nickwanninger@gmail.com>
2020-06-15 21:35:29 -0500

    remove old scripts and fix up some others

--- tools/new_bin.sh
+++ tools/new_bin.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo 'file(GLOB_RECURSE SOURCES RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} *.cpp *.c *.asm)' >> $1/CMakeLists.txt
-echo "chariot_bin($(basename $1))" >> $1/CMakeLists.txt
 
 if [ -z "$1" ];
 then
@@ -13,6 +11,7 @@ fi
 
 mkdir -p user/bin/$1
 
+touch user/bin/$1/CMakeLists.txt
 echo 'file(GLOB_RECURSE SOURCES RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} *.cpp *.c *.asm)' >> user/bin/$1/CMakeLists.txt
 echo "chariot_bin($(basename $1))" >> user/bin/$1/CMakeLists.txt
 
