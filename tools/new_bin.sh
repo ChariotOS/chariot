23d18b8f82caf12ed4bced41eeede6ecba22487b
Nick Wanninger <nickwanninger@gmail.com>
2020-07-07 21:55:56 -0500

    new_bin tool now plops them in the right folder :^)

--- tools/new_bin.sh
+++ tools/new_bin.sh
@@ -9,10 +9,10 @@ fi
 
 
 
-mkdir -p user/bin/$1
+mkdir -p usr.bin/$1
 
-touch user/bin/$1/CMakeLists.txt
-echo 'file(GLOB_RECURSE SOURCES RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} *.cpp *.c *.asm)' >> user/bin/$1/CMakeLists.txt
-echo "chariot_bin($(basename $1))" >> user/bin/$1/CMakeLists.txt
+touch usr.bin/$1/CMakeLists.txt
+echo 'file(GLOB_RECURSE SOURCES RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} *.cpp *.c *.asm)' >> usr.bin/$1/CMakeLists.txt
+echo "chariot_bin($(basename $1))" >> usr.bin/$1/CMakeLists.txt
 
-touch user/bin/$1/main.cpp
+touch usr.bin/$1/main.cpp
