943f1ba49b01e697e7c82c248f98886e3d2b75d7
Nick Wanninger <nickwanninger@gmail.com>
2020-07-21 16:42:14 -0500

    some work on a linereader interface

---
+++ libc/src/ck/linereader.cpp
@@ -0,0 +1,26 @@
+#include <ck/linereader.h>
+#include <stdio.h>
+
+
+
+ck::linereader::linereader(ck::stream &s) : m_stream(s) {}
+
+
+ck::option<ck::string> ck::linereader::next(void) {
+  ck::string buf;
+
+  while (!m_stream.eof()) {
+    int c = m_stream.getc();
+		if (c == EOF) break;
+		if (c == '\n') break;
+    buf.push(c);
+  }
+
+  return Some(buf);
+}
+
+ck::option<ck::string> ck::linereader::next(const ck::string &prompt) {
+  printf("%s", prompt.get());
+  fflush(::stdout);
+	return next();
+}
