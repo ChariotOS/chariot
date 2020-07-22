1e07b5b58f01ebb1444b39b46ef86fd0f294fc57
Nick Wanninger <nickwanninger@gmail.com>
2020-07-22 10:52:03 -0500

    playing around with `ct`

--- usr.bin/ct/main.cpp
+++ usr.bin/ct/main.cpp
@@ -3,6 +3,7 @@
 #include <gfx/rect.h>
 #include <gfx/scribe.h>
 #include <lumen.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,70 +29,36 @@ class painter : public ui::view {
   int color = 0;
 
  public:
-  painter(int width = -1) {
-    if (width != -1) {
-      // set the width
-      set_width_policy(ui::size_policy::fixed);
-      set_size(ui::direction::horizontal, width);
-    }
-    color = rand();
-  }
+  painter(void) { color = rand(); }
 
 
   virtual void paint_event(void) override {
     auto s = get_scribe();
     s.clear(color);
+
+    // s.draw_frame(gfx::rect(0, 0, width(), height()), color);
+
+    // gfx::rect r(10, 10, 50, 50);
+    // s.fill_rect(r, 0x00FF00);
+    // s.draw_rect(r.intersect(gfx::rect(10, 10, 60, 60)), 0xFF0000);
+
     invalidate();
   }
 
-	virtual void on_mouse_move(ui::mouse_event &ev) override {
-      auto s = get_scribe();
+  virtual void on_mouse_move(ui::mouse_event& ev) override {
+    auto s = get_scribe();
 
-			int ox = ev.x - ev.dx;
-			int oy = ev.y - ev.dy;
-      s.draw_line_antialias(ox, oy, ev.x, ev.y, 0);
-			invalidate();
-	}
-
-	/*
-  virtual bool mouse_event(ui::mouse_event& ev) override {
-    active = ev.left;
-
-    if (ev.right) {
-      auto s = get_scribe();
-      s.clear(color);
-      invalidate();
-    }
+    int ox = ev.x - ev.dx;
+    int oy = ev.y - ev.dy;
+    s.draw_line(ox, oy, ev.x, ev.y, 0xFFFFFF);
 
 
-    if (ev.left) {
-      if (!active) {
-        ox = ev.x;
-        oy = ev.y;
-      }
-      auto s = get_scribe();
 
-      s.draw_line_antialias(ox, oy, ev.x, ev.y, 0);
-      invalidate();
-      active = true;
-    } else {
-      active = false;
-    }
-
-    ox = ev.x;
-    oy = ev.y;
-
-    return true;
+    invalidate();
   }
-	*/
 };
 
 
-
-
-struct Thingy {
-  int foo(void) { return 42; }
-};
 
 int main(int argc, char** argv) {
   /*
@@ -117,24 +84,17 @@ int main(int argc, char** argv) {
     ui::application app;
     ck::vec<ui::window*> windows;
 
-    int width = 400;
+    int width = 300;
     int height = 300;
 
 
     char buf[50];
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 5; i++) {
       sprintf(buf, "[Window %d]", i);
       auto win = app.new_window(buf, width, height);
       windows.push(win);
       auto& stk = win->set_view<ui::stackview>(ui::direction::horizontal);
 
-
-			srand(0);
-      stk.spawn<painter>(20);
-      stk.spawn<painter>(50);
-      stk.spawn<painter>(10);
-      stk.spawn<painter>();
-      stk.spawn<painter>(50);
       stk.spawn<painter>();
     }
 
