ef9e0dee6770e18b99cc0326f1a71b3e00ccf6a0
Nick Wanninger <nickwanninger@gmail.com>
2020-08-16 07:16:58 -0500

    add sys/compiler.h which contains some useful gcc macros

---
+++ usr.include/sys/compiler.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+ * This file provides useful GCC macros
+ */
+
+#define weak __attribute__((__weak__))
+#define hidden __attribute__((__visibility__("hidden")))
+#define weak_alias(old, new) \
+	extern __typeof(old) new __attribute__((__weak__, __alias__(#old)))
