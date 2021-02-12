a1346940a9184b2fe23315e948c85e3316d4bd01
Nick Wanninger <nickwanninger@gmail.com>
2021-02-11 21:55:47 -0600

    read: return EINVAL when invalid address is supplied

--- kernel/syscalls/read.cpp
+++ kernel/syscalls/read.cpp
@@ -5,7 +5,7 @@
 ssize_t sys::read(int fd, void *data, size_t len) {
   int n = -1;
 
-  if (!curproc->mm->validate_pointer(data, len, PROT_WRITE)) return -1;
+  if (!curproc->mm->validate_pointer(data, len, PROT_WRITE)) return -EINVAL;
 
   ref<fs::file> file = fget(fd);
   if (file) {
