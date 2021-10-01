4b1f6e8ed8e5f562037e2f2c9b1e23f442087690
Nick Wanninger <nickwanninger@gmail.com>
2021-10-01 11:35:15 -0500

    Print more information before initializing modules

--- include/chariot/module.h
+++ include/chariot/module.h
@@ -5,15 +5,18 @@ typedef void (*init_function_type)(void);
 
 struct kernel_module_info {
   const char *name;
+  const char *file;
   init_function_type initfn;
 };
 
-#define module_init(name, initfn)                                                          \
-  static char __kmod_info_name[] = name;                                                   \
-  static struct kernel_module_info __kmod_info__ __attribute__((__used__))                 \
-      __attribute__((unused, __section__("_kernel_modules"), aligned(sizeof(void *)))) = { \
-          __kmod_info_name,                                                                \
-          initfn,                                                                          \
+#define module_init(name, initfn)                                                      \
+  static char __kmod_info_name[] = name;                                               \
+  static char __kmod_info_file[] = __FILE__;                                           \
+  static struct kernel_module_info __kmod_info__ __attribute__((__used__))             \
+  __attribute__((unused, __section__("_kernel_modules"), aligned(sizeof(void *)))) = { \
+      __kmod_info_name,                                                                \
+      __kmod_info_file,                                                                \
+      initfn,                                                                          \
   };
 
 
--- kernel/module.cpp
+++ kernel/module.cpp
@@ -4,12 +4,25 @@
 void initialize_builtin_modules(void) {
   extern struct kernel_module_info __start__kernel_modules[];
   extern struct kernel_module_info __stop__kernel_modules[];
-  struct kernel_module_info *mod = __start__kernel_modules;
-  int i = 0;
-  while (mod != __stop__kernel_modules) {
-    // printk(KERN_DEBUG "Calling builtin module %s\n", mod->name);
-    mod->initfn();
-    // printk(KERN_DEBUG "Module '%s' done.\n", mod->name);
-    mod = &(__start__kernel_modules[++i]);
+
+  {
+    printk(KERN_DEBUG "Kernel Modules being Loaded:\n");
+    struct kernel_module_info *mod = __start__kernel_modules;
+    int i = 0;
+    while (mod != __stop__kernel_modules) {
+
+			printk(KERN_DEBUG "%p - %s\n", mod->initfn, mod->name);
+      mod = &(__start__kernel_modules[++i]);
+    }
+  }
+
+  {
+    struct kernel_module_info *mod = __start__kernel_modules;
+    int i = 0;
+    while (mod != __stop__kernel_modules) {
+      mod->initfn();
+      // printk(KERN_DEBUG "Module '%s' done.\n", mod->name);
+      mod = &(__start__kernel_modules[++i]);
+    }
   }
 }
