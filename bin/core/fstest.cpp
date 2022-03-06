e45369e75fe635276f3c6229dbfd99ff9a6b731a
Nick Wanninger <ncw@u.northwestern.edu>
2022-03-06 12:43:33 -0600

    add a filesystem test utility

--- bin/core/fstest.cpp
+++ bin/core/fstest.cpp
@@ -4,9 +4,12 @@
 
 
 void expect(const char *msg, bool val) {
-	if (!val) {
-
-	}
+  if (val) {
+    fprintf(stderr, "\e[0;32m[PASS]\e[0m %s\n", msg);
+  } else {
+    fprintf(stderr, "\e[0;31m[FAIL]\e[0m %s\n", msg);
+    exit(EXIT_FAILURE);
+  }
 }
 
 int main(int argc, char **argv) {
@@ -14,8 +17,14 @@ int main(int argc, char **argv) {
     fprintf(stderr, "fstest: provided no paths\n");
     exit(EXIT_FAILURE);
   }
-	ck::file f;
-	expect("open", f.open(argv[1], O_RDWR | O_CREAT, 0644) == 0);
+  {
+    ck::file f;
+    expect("Open", f.open(argv[1], O_RDWR | O_CREAT, 0644));
+  }
 
-	return 0;
+  expect("Unlink", unlink(argv[1]) == 0);
+
+
+
+  return 0;
 }
