bc334b987083693f802a0163caa657d77a88e017
Nick Wanninger <nickwanninger@gmail.com>
2022-09-30 15:49:01 -0500

    use gdb-multiarch

--- tools/gdb.rv.sh
+++ tools/gdb.rv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-riscv64-gdb build/chariot.elf -iex "target remote localhost:1234"
+# riscv64-gdb build/chariot.elf -iex "target remote localhost:1234"
+gdb-multiarch build/chariot.elf -iex "target remote localhost:1234"
